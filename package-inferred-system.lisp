3f09b87174e539672dd036983e6ebcd3182f4bee
Francois-Rene Rideau <fare@tunes.org>
2019-12-10 14:33:40 -0500

    Package local-nicknames on ABCL, CCL, ECL, CLASP
    Fixes #14 as suggested by reporter Andriy Kmit.

--- package-inferred-system.lisp
+++ package-inferred-system.lisp
@@ -75,7 +75,7 @@ the DEFPACKAGE-FORM uses it or imports a symbol from it."
                  (dolist (p arguments) (dep (string p))))
                 ((:import-from :shadowing-import-from)
                  (dep (string (first arguments))))
-                #+sbcl
+                #+(or abcl ccl clasp ecl sbcl)
                 ((:local-nicknames)
                  (loop* :for (local-nickname actual-package-name) :in arguments :do
                       (dep (string actual-package-name))))
