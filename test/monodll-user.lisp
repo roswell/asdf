783c0c1e2b4dd639a774de24f744e8f06cf04cc1
Marius Gerbershagen <marius.gerbershagen@gmail.com>
2026-06-14 16:54:47 +0200

    tests: fix test-bundle for ECL
    ECL's ffi interface needs explicit declarations for C functions.

--- test/monodll-user.lisp
+++ test/monodll-user.lisp
@@ -1,4 +1,5 @@
 (defpackage :test-asdf/monodll-user (:use)) ;; dummy, for package-inferred-system dependencies.
 
 (in-package :asdf-test)
+(ffi:clines "extern int always_42(void);")
 (ffi:def-function "always_42" () :returning :int)
